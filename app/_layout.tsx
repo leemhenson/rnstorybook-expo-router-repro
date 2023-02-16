48b1e8b3edbcd9a6dbfbd31ceb9ae192fdb2a3e9
Lee Henson <lee.m.henson@gmail.com>
2023-02-16 20:18:19 +0000

    with _layout

---
+++ app/_layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import { Stack } from "expo-router";
+
+function Layout() {
+  return (
+    <View style={{ flex: 1 }}>
+      <Stack />
+    </View>
+  );
+}
+
+export default Layout;
