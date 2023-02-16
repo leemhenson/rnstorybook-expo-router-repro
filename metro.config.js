bef904dfc5c7a08d22cad20c0ec64b2f7f23cf46
Lee Henson <lee.m.henson@gmail.com>
2023-02-16 20:14:50 +0000

    example

---
+++ metro.config.js
@@ -0,0 +1,14 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require("expo/metro-config");
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+module.exports = {
+  ...defaultConfig,
+  resolver: {
+    resolverMainFields: [
+      "sbmodern",
+      ...defaultConfig.resolver.resolverMainFields,
+    ],
+  },
+};
